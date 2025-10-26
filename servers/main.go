aa876e078572f862adf379b9920b3bc10d421fcd
Alvin Shi <alvinzyshi@gmail.com>
2025-10-26 19:14:14 +1100

    move endpoint to /app

--- servers/main.go
+++ servers/main.go
@@ -8,10 +8,15 @@ import (
 func main() {
 	fmt.Println("Starting server")
 	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain;charset=utf-8")
+		w.Write([]byte("OK"))
+	})
+
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
-	mux.Handle("/", http.FileServer(http.Dir(".")))
 	server.ListenAndServe()
 }
