8b0e13f3145a6a8f67e2caa42bb26983835621c8
Alvin Shi <alvinzyshi@gmail.com>
2025-10-28 01:39:37 +1100

    strip handlers out of main

--- servers/main.go
+++ servers/main.go
@@ -17,23 +17,29 @@ func (state *apiState) middlewareMetricsInc(next http.Handler) http.Handler {
 	})
 }
 
+func (state *apiState) handlerMetrics(w http.ResponseWriter, req *http.Request) {
+	hits := fmt.Sprintf("Hits: %v", state.fileserverHits.Load())
+	w.Write([]byte(hits))
+}
+func (state *apiState) handlerReset(w http.ResponseWriter, req *http.Request) {
+	state.fileserverHits.Store(0)
+}
+func handlerHealthCheck(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain;charset=utf-8")
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	fmt.Println("Starting server")
-	state := &apiState{}
+	state := &apiState{
+		fileserverHits: atomic.Int32{},
+	}
 
 	mux := http.NewServeMux()
 	mux.Handle("/app/", state.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
-	mux.HandleFunc("/reset", func(w http.ResponseWriter, req *http.Request) {
-		state.fileserverHits.Store(0)
-	})
-	mux.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
-		hits := fmt.Sprintf("Hits: %v", state.fileserverHits.Load())
-		w.Write([]byte(hits))
-	})
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Set("Content-Type", "text/plain;charset=utf-8")
-		w.Write([]byte("OK"))
-	})
+	mux.HandleFunc("/reset", state.handlerReset)
+	mux.HandleFunc("/metrics", state.handlerMetrics)
+	mux.HandleFunc("/healthz", handlerHealthCheck)
 
 	server := &http.Server{
 		Addr:    ":8080",
